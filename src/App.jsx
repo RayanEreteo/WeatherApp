f6b6fddf2ffef6902f184b9b0065a9907f52205e
RayanEreteo <rayabf5@gmail.com>
2023-10-17 20:19:37 +0200

    Design de l'input terminée

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Box, Input, Flex, Text, Heading } from "@chakra-ui/react"
+import { Box, Input, Flex, Text, Heading, Button } from "@chakra-ui/react"
 
 function App() {
 
@@ -6,9 +6,10 @@ function App() {
     <Flex id="main-container" background={"#19191f"} w={"100vw"} h={"100vh"} alignItems={"center"} justifyContent={"center"} flexDirection={"column"}>
       <Heading as={"h1"} color={"white"} mb={"3"}>Info Météo</Heading>
       <Flex id="app-container" padding={"6"} w={"500px"} background={"#fff"} borderRadius={"3xl"}>
-        <form action="">
-          <Input placeholder="Rentrer une ville...." background={"rgba(0,0,0,0.2)"}/>
-        </form>
+        <Flex id="input-container" alignItems={"center"} justifyContent={"space-around"} w={"100%"}>
+          <Input variant={"flushed"} placeholder="Rentrer une ville...." w={"200px"}/>
+          <Button>Vérifier la météo</Button>
+        </Flex>
       </Flex>
     </Flex>
   )
