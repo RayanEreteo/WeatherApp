d59213a7823c5f7da000b7202367395d23bc275f
RayanEreteo <rayabf5@gmail.com>
2023-10-17 19:28:43 +0200

    Input terminée, ajout du font

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import { Box, Input, Flex, Text } from "@chakra-ui/react"
+import { Box, Input, Flex, Text, Heading } from "@chakra-ui/react"
 
 function App() {
 
   return (
-    <div className="app"></div>
+    <Flex id="main-container" background={"#19191f"} w={"100vw"} h={"100vh"} alignItems={"center"} justifyContent={"center"} flexDirection={"column"}>
+      <Heading as={"h1"} color={"white"} mb={"3"}>Info Météo</Heading>
+      <Flex id="app-container" padding={"6"} w={"500px"} background={"#fff"} borderRadius={"3xl"}>
+        <form action="">
+          <Input placeholder="Rentrer une ville...." background={"rgba(0,0,0,0.2)"}/>
+        </form>
+      </Flex>
+    </Flex>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+
+import "@fontsource/montserrat";
+
+const theme = extendTheme({
+  fonts: {
+    heading: "Montserrat, helvetica",
+    body: "Montserrat, helvetica"
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>,
