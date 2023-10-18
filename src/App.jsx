5878c1660291a13e484094bf593d56f501130a7a
RayanEreteo <rayabf5@gmail.com>
2023-10-18 12:37:56 +0200

    Design terminée

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   const API_KEY = ""
   const INPUT_REF = useRef()
   
-  const [mainHeight, setMainHeight] = useState("300px") //! Valeur defaut 100px
+  const [mainHeight, setMainHeight] = useState("400px") //! Valeur defaut 100px
   const [alreadyFetched, setAlreadyFetched] = useState(false)
 
 
@@ -15,7 +15,7 @@ function App() {
     if (INPUT_REF.current.value == null) return
 
     if (!alreadyFetched) {
-      setMainHeight("300px")
+      setMainHeight("400px")
       setAlreadyFetched(true)
     }
 
@@ -25,7 +25,7 @@ function App() {
   return (
     <Flex id="main-container" background={"#19191f"} w={"100vw"} h={"100vh"} alignItems={"center"} justifyContent={"center"} flexDirection={"column"}>
       <Heading as={"h1"} color={"white"} mb={"3"}>Info Météo</Heading>
-      <Flex id="app-container" padding={"6"} w={"500px"} h={mainHeight} background={"#fff"} borderRadius={"3xl"} transition={"all"} transitionDuration={"0.3s"} display={'flex'} alignItems={"flex-start"} flexDirection={"column"}>
+      <Flex id="app-container" padding={"6"} w={"500px"} h={mainHeight} overflow={"hidden"} background={"#fff"} borderRadius={"3xl"} transition={"all"} transitionDuration={"0.3s"} display={'flex'} alignItems={"flex-start"} flexDirection={"column"}>
         <Flex id="input-container" alignItems={"center"} justifyContent={"space-around"} w={"100%"}>
           <InputGroup w={"100%"}>
             <Input variant={"flushed"} placeholder="Rentrer une ville...." _placeholder={{color: "black"}} ref={INPUT_REF}/>
@@ -37,6 +37,10 @@ function App() {
         <Flex id="degrée-container" flexDirection={"column"} alignItems={"center"} justifyContent={"center"} fontSize={"2rem"} m={"auto"}>
           <Text id="degrée">20°C</Text>
           <Image src="https://openweathermap.org/img/wn/10d@2x.png"></Image>
+        </Flex>
+        <Flex id="weather-info-container" flexDirection={"row"} alignItems={"center"} justifyContent={"space-between"} fontSize={"2rem"} w={"100%"}>
+          <Text fontSize={"1.1rem"} id="wind-speed">Vitesse de vent : 14 km/h</Text>
+          <Text fontSize={"1.1rem"} id="humidity">Humidité : 10%</Text>
         </Flex>
       </Flex>
     </Flex>
